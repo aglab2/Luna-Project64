afd49086a1b956c94982b0dac4adfcb9f78d341f
LunaticShiN3 <96666614+LunaticShiN3@users.noreply.github.com>
2023-04-06 21:43:53 +0200

    Updated version name to v3b

--- Source/Project64/main.cpp
+++ Source/Project64/main.cpp
@@ -17,7 +17,7 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /
         // Create the main window with menu
 		
         WriteTrace(TraceUserInterface, TraceDebug, "Create main window");
-        CMainGui MainWindow(true, stdstr_f("Project64 3.0.1-N-v3a").c_str()), HiddenWindow(false);
+        CMainGui MainWindow(true, stdstr_f("Project64 3.0.1-N-v3b").c_str()), HiddenWindow(false);
         CMainMenu MainMenu(&MainWindow);
         CDebuggerUI Debugger;
         g_Debugger = &Debugger;
