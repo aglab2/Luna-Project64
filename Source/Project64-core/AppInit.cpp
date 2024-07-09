ff24e57e9649c4aa200bfea675513af2a56c9906
LunaticShiN3 <96666614+LunaticShiN3@users.noreply.github.com>
2024-07-09 21:25:35 +0200

    Updated version name to v3.5.1

--- Source/Project64-core/AppInit.cpp
+++ Source/Project64-core/AppInit.cpp
@@ -171,7 +171,7 @@ void TraceDone(void)
 
 const char * AppName(void)
 {
-    static stdstr_f ApplicationName("Luna's Project64 v3.5.0");
+    static stdstr_f ApplicationName("Luna's Project64 v3.5.1");
     return ApplicationName.c_str();
 }
 
