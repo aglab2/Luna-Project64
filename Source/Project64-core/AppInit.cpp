538fdfbc87f1445603a0f5ba2781763c1903ff88
aglab2 <aglab3@gmail.com>
2025-03-30 19:20:52 +0800

    Version==3.5.7

--- Source/Project64-core/AppInit.cpp
+++ Source/Project64-core/AppInit.cpp
@@ -171,7 +171,7 @@ void TraceDone(void)
 
 const char * AppName(void)
 {
-    static stdstr_f ApplicationName("Luna's Project64 v3.5.6");
+    static stdstr_f ApplicationName("Luna's Project64 v3.5.7");
     return ApplicationName.c_str();
 }
 
