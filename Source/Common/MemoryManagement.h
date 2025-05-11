34f26324e2b6a5e6b1b3a9ed946d3a42f7b42d4f
aglab2 <aglab3@gmail.com>
2025-05-11 16:46:10 +0800

    Use only low memory, added ClangCL back

--- Source/Common/MemoryManagement.h
+++ Source/Common/MemoryManagement.h
@@ -8,7 +8,7 @@ enum MEM_PROTECTION
     MEM_EXECUTE_READWRITE,
 };
 
-void* AllocateAddressSpaceLow(size_t size, void* base_address = 0);
+#define AllocateAddressSpaceLow AllocateAddressSpace
 void* AllocateAddressSpace(size_t size, void * base_address = 0);
 bool FreeAddressSpace(void* addr, size_t size);
 void* CommitMemory(void* addr, size_t size, MEM_PROTECTION memProtection);
